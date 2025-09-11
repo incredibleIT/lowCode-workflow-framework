a84a5e1c576bce271ad2e5365b9ebda8dced36d8
yangyang1 <yangyang02@bamboocloud.cn>
2025-09-11 09:24:34 +0800

    20250911-定时调度任务处理器处理指定次数定时任务

--- workflow-runner/src/main/java/com/lowcode/workflow/runner/runtime/TimeTaskRuntime.java
+++ workflow-runner/src/main/java/com/lowcode/workflow/runner/runtime/TimeTaskRuntime.java
@@ -2,8 +2,13 @@ package com.lowcode.workflow.runner.runtime;
 
 
 import com.lowcode.workflow.runner.node.timer.TimerNode;
+import org.springframework.scheduling.Trigger;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.support.CronTrigger;
+import org.springframework.scheduling.support.PeriodicTrigger;
 
+import java.time.ZoneId;
+import java.util.TimeZone;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -35,7 +40,7 @@ public class TimeTaskRuntime {
         // 获取执行当前定时器的线程池
         ThreadPoolTaskScheduler schedulerThreadPool = RuntimeDataThreadPool.getSchedulerThreadPool(timerNode.getFlowId());
 
-        // 执行定时任务
+        // TODO 执行定时任务
         if (timerNode.getCron() != null) {
             future[0] = schedulerThreadPool.schedule(task, trigger(timerNode));
         } else {
@@ -44,6 +49,19 @@ public class TimeTaskRuntime {
 
 
 
+    }
+
+    private static Trigger trigger(TimerNode timerNode) {
+
+        Trigger trigger = null;
+
+        if (timerNode.getCron() != null) {
+            trigger = new CronTrigger(timerNode.getCron(), ZoneId.systemDefault());
+        } else {
+            trigger = new PeriodicTrigger(timerNode.getPeriod(), timerNode.getTimeUnit());
+        }
+
+        return trigger;
     }
 
     /**
